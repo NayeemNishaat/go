0c3908284f4f703c52c1461eda401daa156e5ead
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-19 02:03:50 +0600

    for loop in go

--- card/main.go
+++ card/main.go
@@ -5,8 +5,21 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades" // Note: Initialize new variable
 
-	card := "Ace of Spades"   // Note: Initialize new variable
-	card = "Five of Diamonds" // Note: Reassign value
+	// card := "Ace of Spades" // Note: Initialize new variable
+	// card = "Five of Diamonds" // Note: Reassign value
 
-	fmt.Println(card)
+	// card := newCard()
+
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
