907d35a1e9899ca667ad8d0c37a8c1528d02b6ca
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-19 00:56:51 +0600

    init

---
+++ card/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades" // Note: Initialize new variable
+
+	card := "Ace of Spades"   // Note: Initialize new variable
+	card = "Five of Diamonds" // Note: Reassign value
+
+	fmt.Println(card)
+}
