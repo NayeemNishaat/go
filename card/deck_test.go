73b7a2b56005a0346b5000c08d393265473a9020
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-24 23:43:52 +0600

    wrote some more tests

--- card/deck_test.go
+++ card/deck_test.go
@@ -3,6 +3,7 @@ package main
 // go env -w GO111MODULE=auto
 
 import (
+	"os"
 	"testing"
 )
 
@@ -12,4 +13,27 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 16 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting.txt")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting.txt")
+
+	loadedDeck := newDeckFromFile("_decktesting.txt")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting.txt")
 }
