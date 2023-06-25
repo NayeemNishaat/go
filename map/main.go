31f817ab28fa181ea090b3344c0353bb1c0b5194
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-25 17:34:58 +0600

    done with map

--- map/main.go
+++ map/main.go
@@ -5,15 +5,28 @@ import "fmt"
 func main() {
 	// var colors map[string]string // Note: [type]value
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	colors["white"] = "#fff"
-	delete(colors, "white")
+	// colors["white"] = "#fff"
+	// delete(colors, "white")
 
-	// colors := map[string]string{
-	// 	"red":   "#f00",
-	// 	"green": "#0f0",
-	// 	"blue":  "#00f",
+	colors := map[string]string{
+		"red":   "#f00",
+		"green": "#0f0",
+		"blue":  "#00f",
+	}
+
+	printMap(colors)
+	// fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for k, v := range c {
+		fmt.Println(k, v)
+
+	}
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
 	// }
-	fmt.Println(colors)
 }
