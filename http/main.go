6ab2b92180b799e027a8436a0500707a52e96542
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-06-30 13:00:29 +0600

    printing res body

--- http/main.go
+++ http/main.go
@@ -10,9 +10,14 @@ func main() {
 	res, err := http.Get("https://google.com")
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(res)
+	fmt.Println(res, res.Body)
+
+	// bs := []byte{}
+	bs := make([]byte, 99999)
+	res.Body.Read(bs)
+	fmt.Println(string(bs))
 }
