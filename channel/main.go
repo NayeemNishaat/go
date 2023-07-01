4ad27773a79eb14d28cfb90f542aa2004b31000c
Nayeem Nishaat <rxnayeemimi@gmail.com>
2023-07-02 00:49:29 +0600

    listening for messages from channel

--- channel/main.go
+++ channel/main.go
@@ -17,14 +17,20 @@ func main() {
 	c := make(chan string)
 
 	for _, link := range links {
-		go checkLink(link, c) // Remark: Creating new go routines
+		go checkLink(link, c) // Remark: Creating new go routines when the child routine is blocked.
 	}
 
-	fmt.Println(<-c)
+	// fmt.Println(<-c) // Important: Blocking call, waiting for messages from the channel
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
+	}
 }
 
 func checkLink(link string, c chan string) {
-	_, err := http.Get(link)
+	_, err := http.Get(link) // Note: Blocking call
 	if err != nil {
 		fmt.Println(link, "might be down!")
 		c <- link + "Down!"
