f1c1992098cab0e72a7ac3de61ba6b0791a11b80
lAbYriNth <47172193+NayeemNishaat@users.noreply.github.com>
2024-02-26 01:11:49 +0600

    Update main.go

--- hello-world/main.go
+++ hello-world/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func init() {
+	fmt.Println("INIT First")
+}
+
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello Last")
+}
+
+func init() {
+	fmt.Println("INIT Second")
 }
